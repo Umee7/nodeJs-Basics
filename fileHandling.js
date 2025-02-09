7df47b73f3fd6f39958ed4ea0b056fa97c6332ae
Umee7 <umeshmessi23@gmail.com>
2025-02-09 12:05:41 +0530

    Made a file fileHandling.js to learn about fs module

---
+++ fileHandling.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.writeFileSync("./firstFile", "I created this file using writeFileSync function in fs module.") 
+// fs.writeFile("./secondFile", "I made this file using writeFile async function", (error) => {});
+// const result = fs.readFileSync("./firstFile", "utf-8");
+// console.log(result)
+// fs.readFile("./secondFile", "utf-8", (err, result) => {
+//     if(err) {
+//         console.log("Error: ", err);
+//     } else {
+//         console.log(result);
+//     }
+// })
+// fs.mkdirSync("./Practice");
+// fs.writeFileSync("./sample.txt", "I made this file to delete")
+// fs.unlinkSync("./sample.txt");
