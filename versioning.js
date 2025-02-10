01a5e42d78963ae68a6b0c72d21a8c50da8c1b78
Umee7 <umeshmessi23@gmail.com>
2025-02-11 00:32:01 +0530

    Learned about version control

---
+++ versioning.js
@@ -0,0 +1,12 @@
+// my current version im using is ^4.21.2
+//  4.21.2
+// 4 -> major/breaking
+// 21 -> recommended bugfixes, security changes
+// 2 -> minor bud fixes (optional)
+// ^4.21.2 => ^made 4 fixed, everthing after 4. can be updated without worries.
+
+// if it is ~4.21.2 => 4.21 is fixed
+// never use latest as version. as it updates major breaking version without consent
+// Basically that's all about version control of node.
+// More things are found at npmjs.com 
+// It is a good habit to check documentation often.
