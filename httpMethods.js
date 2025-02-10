773a37f1bd43e5a2a738238bc4c277d231facff5
Umee7 <umeshmessi23@gmail.com>
2025-02-10 20:33:28 +0530

    Learned about HTTP methods

---
+++ httpMethods.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const http = require('http');
+
+const myServer = http.createServer((req, res)=>{
+    //basically my request method here is GET by default, if i add data it can be POST, PUT, PATCH, DELETE
+    const log = `hello ${req.method} ${req.url}`
+    fs.appendFile('./log.txt', log, (err, data)=>{
+        if(err){
+            console.log("Error: ", err);
+        } else res.end("Done");
+    })
+})
+
+myServer.listen(8001, ()=>console.log("Port working well"));
